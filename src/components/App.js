122b618fb0c890d4d1c3bed4c3034ae2af3a79b0
Jose Villarroel <oscarclarke9900@gmail.com>
2019-02-14 18:25:09 +0100

    added prettify, also added context to manage language app

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
+import UserCreate from './UserCreate'
 
 class App extends Component {
+  state = {
+    language: 'english'
+  }
 
-    state = {
-        language: 'english'
-    }
-
-    render() {
-        console.log(this.state);
-        return (
-            <div className="ui container">
-                <div>
-                    Select language:
-                    <i className="flag us" onClick={() => this.setState({ language: 'english' })} />
-                    <i className="flag es" onClick={() => this.setState({ language: 'spanish' })} />
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select language:
+          <i className="flag us" onClick={() => this.setState({ language: 'english' })} />
+          <i className="flag es" onClick={() => this.setState({ language: 'spanish' })} />
+        </div>
+        <UserCreate />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
